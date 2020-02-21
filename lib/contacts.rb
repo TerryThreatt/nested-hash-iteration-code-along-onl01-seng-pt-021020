12daae8f51acc7a6cb0ada7ecd22749fb06cef99
Terry Threatt <terry.threatt@gmail.com>
2020-02-21 23:28:49 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each |person, contacts_details_hash|
+  contacts.each do |person, contacts_details_hash|
   
   if person == "Freddy Mercury"
     contacts_details_hash.each do |attribute, data|
